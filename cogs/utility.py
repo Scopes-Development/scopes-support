910dbab5708b3e0055935be1b245ae630b3b50e5
ELCScopes <seif.wessam66@gmail.com>
2021-06-15 00:26:34 +0200

    Added /host to view information about [sweplox.net](https://sweplox.net)

--- cogs/utility.py
+++ cogs/utility.py
@@ -20,6 +20,18 @@ class Rules(commands.Cog):
         )
         await ctx.send(embed=embed)
 
+    @cog_ext.cog_slash(name="host", guild_ids=guild_id, description="Know what hosting service we use!")
+    async def host(self, ctx):
+        embed = discord.Embed(color=discord.Color.purple(), title="Sweplox Hosting", url="https://sweplox.net",
+                              description="We use [sweplox.net](https://sweplox.net 'Sweplox Hosting panel') to host all our bots on the lowest ping possible and the best processors!")
+
+        embed.add_field(
+            name="🔗Links", value="[Discord Server](https://discord.gg/XPvZqsHu 'Sweplox Hosting discord server')\n[Game Panel](https://game.panel.sweplox.net 'Game panel/Control panel of your servers on Sweplox Hosting')\n[ARC.io AFK page](https://client.sweplox.net/arcio 'Earn coins and upgrade your resources with them by being AFK on this page.')\n[Client Page](https://client.sweplox.net 'Sweplox Hosting client page')")
+
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/icons/582928406794993664/a_61b8630f5f0759d8d24c74259940c9e5.gif?size=4096")
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Rules(bot))
