440cc6743fde66ae86a65afc749cf06930675127
ELCScopes <seif.wessam66@gmail.com>
2021-06-15 00:15:50 +0200

    replaced help parameter with description

--- cogs/utility.py
+++ cogs/utility.py
@@ -11,7 +11,7 @@ class Rules(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="ping", guild_ids=guild_id, help="Display the bot's latency")
+    @cog_ext.cog_slash(name="ping", guild_ids=guild_id, description="Display the bot's latency")
     async def ping(self, ctx, arg):
         embed = discord.Embed(
             colour=ctx.author.color,
@@ -19,6 +19,7 @@ class Rules(commands.Cog):
             description=f'Latency : **{round(self.bot.latency * 1000)}ms**'
         )
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Rules(bot))
